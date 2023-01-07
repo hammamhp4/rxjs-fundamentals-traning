958a54624dd6d06853ea9f08072995baa7cf3f9f
mohamed KHADHRAOUI <mohamed.khadhraoui@proxym-it.com>
2023-01-07 17:02:15 +0100

    subscription ex

--- applications/basic-counter/script.js
+++ applications/basic-counter/script.js
@@ -1,5 +1,11 @@
-import { fromEvent, interval, merge, NEVER } from 'rxjs';
+import {fromEvent, interval, merge, NEVER, Subscription} from 'rxjs';
 import { setCount, startButton, pauseButton } from './utilities';
 
 const start$ = fromEvent(startButton, 'click');
 const pause$ = fromEvent(pauseButton, 'click');
+let subscription;
+const tick$ = interval(1000);
+start$.subscribe(()=> {
+  subscription = tick$.subscribe(setCount)
+});
+pause$.subscribe(()=> subscription.unsubscribe())
