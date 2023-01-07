23eaa47290c2adbfab5ad478c43d3b60fa9372c7
mohamed KHADHRAOUI <mohamed.khadhraoui@proxym-it.com>
2023-01-07 21:47:08 +0100

    takeUntil skipUntil

--- applications/basic-counter/script.js
+++ applications/basic-counter/script.js
@@ -1,11 +1,20 @@
-import {fromEvent, interval, merge, NEVER, Subscription} from 'rxjs';
+import {fromEvent, interval, merge, NEVER, skipUntil, Subscription} from 'rxjs';
 import { setCount, startButton, pauseButton } from './utilities';
+import {takeUntil, scan} from "rxjs/operators";
 
 const start$ = fromEvent(startButton, 'click');
 const pause$ = fromEvent(pauseButton, 'click');
 let subscription;
 const tick$ = interval(1000);
+/*
 start$.subscribe(()=> {
   subscription = tick$.subscribe(setCount)
 });
 pause$.subscribe(()=> subscription.unsubscribe())
+*/
+let observable$ = tick$.pipe(
+  skipUntil(start$),
+  scan(acc=> acc+1,0),
+  takeUntil(pause$),
+)
+observable$.subscribe(setCount);
