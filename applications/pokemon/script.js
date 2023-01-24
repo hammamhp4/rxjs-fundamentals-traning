8eb6c253fe41d20c5225ff4aeb743e5760d19245
mohamed KHADHRAOUI <mohamed.khadhraoui@proxym-it.com>
2023-01-24 18:01:11 +0100

    calling 2 apis and merge them and use pluck + first

--- applications/pokemon/script.js
+++ applications/pokemon/script.js
@@ -14,6 +14,7 @@ import {
   concat,
   take,
   EMPTY,
+  pluck
 } from 'rxjs';
 
 import { fromFetch } from 'rxjs/fetch';
@@ -25,6 +26,55 @@ import {
   endpointFor,
   search,
   form,
-} from '../pokemon/utilities';
+  renderPokemon,
+} from './utilities';
+import {first} from "rxjs/operators";
 
-const endpoint = 'http://localhost:3333/api/pokemon?delay=100';
+const endpoint = 'http://localhost:3333/api/pokemon/';
+
+const searchPokemon = (searchTerm) => {
+  return fromFetch(endpoint + 'search/' + searchTerm).pipe(
+    mergeMap(response => response.json()),
+  )
+}
+const getPokemonData = (pokemon) => {
+  return fromFetch(endpoint + pokemon.id).pipe(
+    mergeMap(response => response.json()),
+  )
+}
+
+const search$ = fromEvent(form, 'submit').pipe(
+  map(() => search.value),
+  switchMap(searchPokemon),
+  pluck('pokemon'),
+  mergeMap(pokemon => pokemon),
+  // take(1)
+  first(),
+  tap(renderPokemon),
+  switchMap((pokemon)=>{
+    const pokemon$ = of(pokemon);
+    const additionalData$ = getPokemonData(pokemon).pipe(map((data) => {
+      return {
+        ...pokemon,
+        data
+      }
+    }))
+    return merge(pokemon$,additionalData$);
+  }),
+  tap(renderPokemon),
+)
+
+/*
+const oldSearch$ = fromEvent(search, 'input').pipe(
+  map(event => event.target.value),
+  switchMap(textToSearch =>
+    fromFetch(endpoint + textToSearch + '&delay=1000&chaos=true').pipe(
+      mergeMap(response => response.json()),
+    )),
+  tap(clearResults),
+  pluck('pokemon'),
+  tap(addResults)
+);
+*/
+
+search$.subscribe(console.log);
